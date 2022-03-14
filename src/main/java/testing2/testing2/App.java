4b5e2a743d8d33738823b80e23e90db0220fe0ad
SamilamX <30898665+SamilamX@users.noreply.github.com>
2022-03-14 15:51:21 +0100

    test 3

---
+++ src/main/java/testing2/testing2/App.java
@@ -0,0 +1,13 @@
+package testing2.testing2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
